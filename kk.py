7e222d84de02c0beff9ca0fa8c8ae20beaf3ae4e
Geronimo Beco <gcapitabeco@gmail.com>
2025-09-04 20:25:33 +0100

    Limpeza no diretório2

--- kk.py
+++
@@ -1,7 +0,0 @@
-from core.link_excel import *
-import core.entidades as entity
-
-pontos = listar_pontos_turisticos_mais_avaliados()#
-print("Pontos turísticos mais bem avaliados:")
-for ponto in pontos:
-        print(f"ID: {ponto[0]} | Total de Avaliações: {ponto[1]} | Média: {ponto[2]}")
